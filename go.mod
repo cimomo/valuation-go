2c7492077b1b35eccc1e5c4dc7656cdef222b1b0
Kai Chen <kaichen@outlook.com>
2021-03-02 11:28:39 -0800

    WIP: Set up valuation

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/cimomo/valuation-go
 
 go 1.14
 
-require github.com/spf13/cobra v1.1.3
+require (
+	github.com/cimomo/alphavantage-go v0.0.0-20210204200911-24eb45ad8f2c
+	github.com/spf13/cobra v1.1.3
+)
---
+++ pkg/valuation/company.go
@@ -0,0 +1,12 @@
+package valuation
+
+import "github.com/cimomo/alphavantage-go"
+
+// Company defines valuation input data for a company
+type Company struct {
+	Symbol          string
+	Overview        *alphavantage.Company
+	IncomeStatement *alphavantage.IncomeStatement
+	BalanceSheet    *alphavantage.BalanceSheet
+	CashFlow        *alphavantage.CashFlow
+}
