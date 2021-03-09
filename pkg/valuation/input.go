3f85a4cfd77c9701d0a6b1bab2d1ad5125460d59
Kai Chen <kaichen@outlook.com>
2021-03-08 23:19:19 -0800

    WIP: Valuation input

---
+++ pkg/valuation/input.go
@@ -0,0 +1,10 @@
+package valuation
+
+// Input defines the company specific input data for the valuation
+type Input struct {
+	Revenue     float64
+	EBIT        float64
+	TotalEquity float64
+	TotalDebt   float64
+	TotalCash   float64
+}
