0f0b66c997ca55566384937251e2ad1d0311de77
Kai Chen <kaichen@outlook.com>
2021-03-10 22:59:17 -0800

    WIP: cost of capital is company specific

--- pkg/valuation/input.go
+++ pkg/valuation/input.go
@@ -2,10 +2,12 @@ package valuation
 
 // Input defines the company specific input data for the valuation
 type Input struct {
-	Revenue          float64
-	EBIT             float64
-	TotalEquity      float64
-	TotalDebt        float64
-	TotalCash        float64
-	EffectiveTaxRate float64
+	Revenue               float64
+	EBIT                  float64
+	TotalEquity           float64
+	TotalDebt             float64
+	TotalCash             float64
+	EffectiveTaxRate      float64
+	CostOfCapital         float64
+	TerminalCostOfCapital float64
 }
--- pkg/valuation/market.go
+++ pkg/valuation/market.go
@@ -2,9 +2,7 @@ package valuation
 
 // Market defines the broader market and macro data used as valuation input
 type Market struct {
-	RiskFreeRate          float64
-	TerminalRiskFreeRate  float64
-	CostOfCapital         float64
-	TerminalCostOfCapital float64
-	MarginalTaxRate       float64
+	RiskFreeRate         float64
+	TerminalRiskFreeRate float64
+	MarginalTaxRate      float64
 }
