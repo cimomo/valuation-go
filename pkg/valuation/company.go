3a47a553f6bdbb435c922afe46f9d279fee8e9f9
Kai Chen <kaichen@outlook.com>
2021-03-15 22:32:26 -0700

    WIP: AlphaVantage client

--- pkg/valuation/company.go
+++ pkg/valuation/company.go
@@ -5,6 +5,7 @@ import "github.com/cimomo/alphavantage-go"
 // Company defines valuation input data for a company
 type Company struct {
 	Symbol          string
+	Client          *alphavantage.Client
 	Overview        *alphavantage.Company
 	IncomeStatement *alphavantage.IncomeStatement
 	BalanceSheet    *alphavantage.BalanceSheet
