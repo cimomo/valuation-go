3dd339a3c4bade07ddfddfa03c72fed0cc872681
Kai Chen <kaichen@outlook.com>
2021-03-25 14:22:20 -0700

    WIP: Compute TTM revenue

--- pkg/valuation/input.go
+++ pkg/valuation/input.go
@@ -1,5 +1,10 @@
 package valuation
 
+import (
+	"errors"
+	"strconv"
+)
+
 // Input defines the company specific input data for the valuation
 type Input struct {
 	Company               *Company
@@ -21,4 +26,40 @@ func NewInput(company *Company) (*Input, error) {
 	}
 
 	return &input, nil
+}
+
+// Compute computes the valuation input from company fundamentals data
+func (input *Input) Compute() error {
+	err := input.computeRevenue()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (input *Input) computeRevenue() error {
+	quarterly := input.Company.IncomeStatement.QuarterlyReports
+	if quarterly == nil {
+		return errors.New("No quarterly income statement found")
+	}
+
+	if len(quarterly) < 4 {
+		return errors.New("Need at least four quarters of results")
+	}
+
+	ttm := quarterly[:4]
+
+	revenue := 0.0
+	for _, v := range ttm {
+		r, err := strconv.ParseFloat(v.TotalRevenue, 64)
+		if err != nil {
+			return err
+		}
+		revenue += r
+	}
+
+	input.Revenue = revenue
+
+	return nil
 }
