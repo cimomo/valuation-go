c2c78fe689c99ff80d37abb877ad01dd641961cc
Kai Chen <kaichen@outlook.com>
2021-03-29 20:18:59 -0700

    test: Unit test for input

---
+++ pkg/valuation/input_test.go
@@ -0,0 +1,28 @@
+package valuation
+
+import "testing"
+
+func TestComputeInput(t *testing.T) {
+	company, err := NewCompany("MSFT")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = company.Fetch()
+	if err != nil {
+		t.Error(err)
+	}
+
+	input, err := NewInput(company)
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = input.Compute()
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Logf("Revenue TTM: %f", input.Revenue)
+	t.Logf("EBIT TTM: %f", input.EBIT)
+}
