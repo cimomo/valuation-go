3d0e94cd8689bddb90d6904d9db52b719a80517f
Kai Chen <kaichen@outlook.com>
2021-04-12 20:25:38 -0700

    WIP: Market data.

--- pkg/valuation/market.go
+++ pkg/valuation/market.go
@@ -6,3 +6,14 @@ type Market struct {
 	TerminalRiskFreeRate float64
 	MarginalTaxRate      float64
 }
+
+// NewMarket returns a new market object
+func NewMarket(riskFreeRate float64, terminalRiskFreeRate float64, marginalTaxRate float64) (*Market, error) {
+	market := Market{
+		RiskFreeRate:         riskFreeRate,
+		TerminalRiskFreeRate: terminalRiskFreeRate,
+		MarginalTaxRate:      marginalTaxRate,
+	}
+
+	return &market, nil
+}
