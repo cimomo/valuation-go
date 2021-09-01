fb7325312682b1fd96b22ef2ccde92d8e9f4b15b
Kai Chen <kaichen@outlook.com>
2021-08-31 20:50:16 -0700

    test: e2e for year in high growth

--- pkg/valuation/e2e_test.go
+++ pkg/valuation/e2e_test.go
@@ -49,4 +49,12 @@ func TestValuation(t *testing.T) {
 	t.Logf("Base year EBIT: %f", output.BaseYear.EBIT)
 	t.Logf("Base year tax rate: %f", output.BaseYear.TaxRate)
 	t.Logf("Base year NOPAT: %f", output.BaseYear.AfterTaxEBIT)
+
+	for i, year := range output.HighGrowthYears {
+		t.Logf("Year %d revenue: %f", i, year.Revenue)
+		t.Logf("Year %d EBIT margin: %f", i, year.EBITMargin)
+		t.Logf("Year %d EBIT: %f", i, year.EBIT)
+		t.Logf("Year %d tax rate: %f", i, year.TaxRate)
+		t.Logf("Year %d NOPAT: %f", i, year.AfterTaxEBIT)
+	}
 }
