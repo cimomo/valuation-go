03e5e360e7d432f8d51fb22b00da9d87a03a2242
Kai Chen <kaichen@outlook.com>
2022-07-01 18:50:15 -0700

    WIP: stock value command

--- pkg/cmd/value.go
+++ pkg/cmd/value.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 
+	"github.com/cimomo/valuation-go/pkg/valuation"
 	"github.com/spf13/cobra"
 )
 
@@ -27,5 +28,42 @@ func NewValueCmd() *cobra.Command {
 
 func doValue(ticker string) error {
 	fmt.Println("[stock] valuing", ticker)
+
+	company, err := valuation.NewCompany(ticker)
+	if err != nil {
+		return err
+	}
+
+	err = company.Fetch()
+	if err != nil {
+		return err
+	}
+
+	market, err := valuation.NewMarket(0.02, 0.02, 0.25)
+	if err != nil {
+		return err
+	}
+
+	input, err := valuation.NewInput(company, 0.15, 0.07, 0.07, 0.12, 0.4, 0.45, 1.17)
+	if err != nil {
+		return err
+	}
+
+	err = input.Compute()
+	if err != nil {
+		return err
+	}
+
+	output, err := valuation.NewOutput(market, input)
+	if err != nil {
+		return err
+	}
+
+	err = output.Compute()
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Value per share:", output.ValuePerShare)
 	return nil
 }
